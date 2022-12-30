a554fb53e90e092cad2702bbdf9a57133eed0d5c
ali mokhtari <alimokhtari910@yahoo.com>
2022-12-30 19:54:32 +0330

    add image to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import getFormattedWeatherData from './services/weatherService';
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: 'tehran' });
+    console.log(data);
+  };
+
+  fetchWeather();
   return <div className="App">Hello react</div>;
 }
 

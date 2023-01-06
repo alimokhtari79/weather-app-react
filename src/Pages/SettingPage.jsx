8d8ecac8b4f21ea48ae78590ef7c61a48e65511d
ali mokhtari <alimokhtari910@yahoo.com>
2023-01-06 19:54:39 +0330

    add setting page

--- src/Pages/SettingPage.jsx
+++ src/Pages/SettingPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const SettingPage = () => {
   return (
-    <div>
-      <p>This is Setting Page</p>
+    <div className="p-6">
+      <Header title="Setting" />
+      <div className="setting-container flex flex-col items-center my-6">
+        <div className="time text-8xl mb-9">
+          <p>14:25</p>
+        </div>
+        <div className="w-full py-4 px-7 mb-10 flex justify-between rounded-full glassEffect">
+          <p>Temperature based on</p>
+          <p className="text-yellow-400">C</p>
+        </div>
+        <div className="w-full py-4 px-7 mb-10 flex justify-between rounded-full glassEffect">
+          <p>Theme</p>
+          <p className="text-yellow-400">Dark</p>
+        </div>
+      </div>
     </div>
   );
 };

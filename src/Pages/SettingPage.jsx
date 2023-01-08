0891525a28632affa9bd7d5499de44a590aef750
ali mokhtari <alimokhtari910@yahoo.com>
2023-01-08 23:38:05 +0330

    dynamic setting

--- src/Pages/SettingPage.jsx
+++ src/Pages/SettingPage.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
 import Header from '../components/Header';
+import { formatToLocalTime } from '../services/weatherService';
 
-const SettingPage = () => {
+const SettingPage = ({ weather, setUnits, isMetric, setIsMetric }) => {
+  const handelChangeUnits = () => {
+    setIsMetric(!isMetric);
+    if (isMetric) {
+      setUnits('metric');
+    } else {
+      setUnits('imperial');
+    }
+  };
+
   return (
     <div className="p-6">
       <Header title="Setting" />
       <div className="setting-container flex flex-col items-center my-6">
         <div className="time text-8xl mb-9">
-          <p>14:25</p>
+          <p>
+            {weather
+              ? formatToLocalTime(weather.dt, weather.timezone, 'hh: mm')
+              : ''}
+          </p>
         </div>
-        <div className="w-full py-4 px-7 mb-10 flex justify-between rounded-full glassEffect">
+        <div
+          onClick={handelChangeUnits}
+          className="w-full py-4 px-7 mb-10 flex justify-between rounded-full glassEffect"
+        >
           <p>Temperature based on</p>
-          <p className="text-yellow-400">C</p>
+          <p className="text-yellow-400">{isMetric ? 'F' : 'C'}</p>
         </div>
         <div className="w-full py-4 px-7 mb-10 flex justify-between rounded-full glassEffect">
           <p>Theme</p>

2c579c9cb453f201ea6708e4ae40f22ba816915a
ali mokhtari <alimokhtari910@yahoo.com>
2023-01-08 23:40:09 +0330

    update

--- src/Pages/ExplorePage.jsx
+++ src/Pages/ExplorePage.jsx
@@ -1,13 +1,15 @@
 import Header from '../components/Header';
 import Explore from './../components/Explore';
 
-const ExplorePage = ({ cities }) => {
+const ExplorePage = ({ cities, setQuery }) => {
   return (
     <div className="p-6">
       <Header title="Explore" />
       <div className="explore-container grid my-6">
         {cities &&
-          cities.map((city) => <Explore key={city.name} city={city} />)}
+          cities.map((city) => (
+            <Explore key={city.name} city={city} setQuery={setQuery} />
+          ))}
       </div>
       <div className="mb-16"></div>
     </div>
